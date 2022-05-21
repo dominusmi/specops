0e0c5bb517f6e41d1569ee6d1ab96fcd7215e09b
Ed <barp.edoardo@gmail.com>
2022-05-21 13:33:02 +0200

    Add rigid body for wall and `spawn_wall` system

--- src/tilemap.rs
+++ src/tilemap.rs
@@ -8,6 +8,10 @@ use bevy::prelude::*;
 use crate::{ascii::{spawn_ascii_sprite, AsciiSheet}, TILE_SIZE, RESOLUTION};
 use bevy::input::ElementState;
 use bevy_mouse_tracking_plugin::{MousePosWorld, MainCamera};
+use bevy::utils::tracing::Instrument;
+use bevy_rapier2d::prelude::RigidBody;
+use bevy_rapier2d::geometry::Collider;
+use crate::player::Player;
 
 pub struct TileMapPlugin;
 
@@ -22,27 +26,39 @@ impl Plugin for TileMapPlugin {
 }
 
 fn create_simple_map(mut commands: Commands, ascii: Res<AsciiSheet>) {
-    let mut tiles = Vec::new();
+    let tile = spawn_ascii_sprite(
+        &mut commands,
+        &ascii,
+        12*16+4 as usize,
+        Color::rgb(0.9, 0.9, 0.9),
+        Some(Vec2::splat(TILE_SIZE)),
+        Vec3::new(0.0, -0.3, 0.0),
+    );
+    commands.entity(tile)
+        .insert(RigidBody::Fixed)
+        .insert(Collider::cuboid(TILE_SIZE/2.0, TILE_SIZE/8.0));
 
-    for x in 0..20 {
-        for y in 0..100 {
-            let tile = spawn_ascii_sprite(
-                &mut commands,
-                &ascii,
-                12*16+4 as usize,
-                Color::rgb(0.9, 0.9, 0.9),
-                Vec3::new(x as f32 * TILE_SIZE, -(y as f32) * TILE_SIZE, 100.0),
-            );
-            tiles.push(tile);
-        }
-    }
-
-    commands
-        .spawn()
-        .insert(Name::new("Map"))
-        .insert(Transform::default())
-        .insert(GlobalTransform::default())
-        .push_children(&tiles);
+    // let mut tiles = Vec::new();
+    // for x in 0..20 {
+    //     for y in 4..100 {
+    //         let tile = spawn_ascii_sprite(
+    //             &mut commands,
+    //             &ascii,
+    //             12*16+4 as usize,
+    //             Color::rgb(0.9, 0.9, 0.9),
+    //             Some(Vec2::splat(TILE_SIZE)),
+    //             Vec3::new(x as f32 * TILE_SIZE, -(y as f32) * TILE_SIZE, 100.0),
+    //         );
+    //         tiles.push(tile);
+    //     }
+    // }
+    //
+    // commands
+    //     .spawn()
+    //     .insert(Name::new("Map"))
+    //     .insert(RigidBody::Fixed)
+    //     .insert(Collider::cuboid(TILE_SIZE/2.0, TILE_SIZE/8.0))
+    //     .push_children(&tiles);
 }
 
 fn spawn_wall(
@@ -51,11 +67,12 @@ fn spawn_wall(
     ascii: Res<AsciiSheet>,
     mouse: Res<MousePosWorld>,
     window: Res<WindowDescriptor>,
-    camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>
+    player_query: Query<(&Player, &Transform)>
 ){
+    let (player, transform): (&Player, &Transform) = player_query.single();
     if event.just_pressed(MouseButton::Right){
-        let x = ((mouse.x / window.width) * 2.0 - 1.) * RESOLUTION;
-        let y = (mouse.y / window.height) * 2.0 - 1.;
+        let x = ((mouse.x / window.width) * 2.0 - 1.) * RESOLUTION + transform.translation.x;
+        let y = ((mouse.y / window.height) * 2.0 - 1.) + transform.translation.y;
         let position = Vec3::new(x,y,0.0);
 
         let tile = spawn_ascii_sprite(
@@ -63,9 +80,12 @@ fn spawn_wall(
             &ascii,
             12*16+4 as usize,
             Color::rgb(0.9, 0.9, 0.9),
+            Some(Vec2::splat(TILE_SIZE)),
             position,
         );
         info!("Spawning tile at {}", position);
-        commands.entity(tile);
+        commands.entity(tile)
+            .insert(RigidBody::Fixed)
+            .insert(Collider::cuboid(TILE_SIZE/2.0, TILE_SIZE/8.0));
     }
 }
